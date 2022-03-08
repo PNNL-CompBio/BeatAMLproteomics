89fd4b6f31876c842293a20ccb193dbe370d1eb3
Sara Gosline <sara.gosline@pnnl.gov>
2022-03-08 15:29:46 -0800

    updates for additional deconv work

--- tumorDeconv/01-formatDataForDeconv.py
+++ tumorDeconv/01-formatDataForDeconv.py
@@ -12,10 +12,14 @@ from pybeataml import load_data
 import pandas as pd
 
 data = load_data.AMLData()
-mat = pd.pivot(data.proteomics,index='gene_symbol',columns='sample_id',values='exp_value')
-mat.to_csv('AML-tumor-prot-raw.tsv',sep='\t')
+mat = pd.pivot(data.proteomics, index='gene_symbol', \
+               columns='sample_id', values='exp_value')
+mat.to_csv('AML-tumor-prot-raw.tsv', sep='\t')
 
 ##write out proteomics files
 
+mat = pd.pivot(data.rna, index='gene_symbol', \
+               columns='sample_id', values='exp_value')
+mat.to_csv('AML-tumor-mrna-raw.tsv', sep='\t')
 #bdb_file = 'http://'
 #sig_mat_file = ''
