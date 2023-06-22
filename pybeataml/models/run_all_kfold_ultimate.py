9530d881a43e4053c924c3f834d37d01b2b4e5a1
James <james.ch.pino@gmail.com>
2023-06-22 12:53:36 -0400

    Adding files used to generate subtypes with lightGBM. Adding notebooks for paper.

--- pybeataml/models/run_all_kfold_ultimate.py
+++ pybeataml/models/run_all_kfold_ultimate.py
@@ -2,10 +2,9 @@ import lightgbm as lgb
 import numpy as np
 import pandas as pd
 import sklearn.linear_model as LM
-from scipy.stats import pearsonr
+from scipy.stats import pearsonr, spearmanr
 from sklearn import metrics
-from sklearn import svm
-from sklearn.model_selection import KFold
+from sklearn.model_selection import RepeatedKFold
 
 from pybeataml.load_data import AMLData
 
@@ -20,140 +19,155 @@ en_model = LM.ElasticNet(
     alpha=.9,
 )
 
-lasso_model = LM.Lasso(
-    alpha=0.1,
-    max_iter=100000,
-    fit_intercept=True,
-)
 
-svm_model = svm.SVR(C=1.0, epsilon=0.2, kernel='linear')
-svc_model = svm.SVC(C=1.0, kernel='linear')
+# svm_model = svm.SVR(C=100.0, epsilon=0.2, kernel='linear')
 
 
-def run_sklearn(x_train, y_train, x_test, y_test, model, model_name, binarize=False):
-    # don't think we need scaling? Can add in pretty quickly if we do
-    #     pipe = pipeline.Pipeline([
-    # #         ('scaler', preprocessing.StandardScaler()),
-    #         ('model', model)
-    #     ])
-    if binarize:
-        y_train, y_test = convert_to_binary(y_train, y_test)
-
+def run_sklearn(x_train, y_train, x_test, y_test, model, model_name):
     model.fit(x_train, y_train)
     preds = model.predict(x_test)
-    error, r2, pearson = score_all(y_test, preds)
     coef = np.array(model.coef_).flatten()
-    feature_names = model.feature_names_in_[coef > 0]
-    auc = np.nan
-    if binarize:
-        auc = metrics.average_precision_score(y_test, preds)
+    selected_feat = model.feature_names_in_[coef > 0]
+    error, r2, pearson, spearman, pr, sr = score_all(y_test, preds)
+
     return {
-        'pearsonr': pearsonr(y_test, preds)[0],
-        'mse': error,
-        'r2': r2,
         'model': model_name,
-        'feature_names': feature_names,
-        'auc': auc
+        'feature_names': sorted(selected_feat),
+        'n_feat': len(selected_feat),
+        'rmse': error,
+        'r2': r2,
+        'pearson': pearson,
+        'spearman': spearman,
+        'pr': pr,
+        'sr': sr,
     }
 
 
-def convert_to_binary(y_train, y_test):
-    """ Binarize AUC """
-    y_train_c = np.copy(y_train)
-    y_test_c = np.copy(y_test)
-    y_train_c[y_train_c < 100] = 1
-    y_train_c[y_train_c > 100] = 0
-    y_test_c[y_test_c < 100] = 1
-    y_test_c[y_test_c > 100] = 0
-    return y_train_c, y_test_c
+good_param = dict(
+    device_type='cpu',
+    boosting_type='gbdt',
+    num_threads=8,
+    n_jobs=None,
+    objective='regression',
+    metric='rmse',
+    lambda_l1=100,
+    lambda_l2=10,
+    reg_alpha=None,
+    reg_lambda=None,
+    learning_rate=.1,
+
+    max_bin=128,
+    num_leaves=21,  # 21
+    max_depth=-1,  # -1
+
+    zero_as_missing=True,
+
+    feature_fraction=.8,  # .8
+
+    bagging_freq=1,
+    bagging_fraction=.75,
+    subsample_freq=None,
+    subsample=None,
+
+    min_data_in_leaf=1,
+    min_child_samples=None,
+    colsample_bytree=None,
+    min_split_gain=None,
+    n_estimators=10000,
+    verbose=-1,
+    deterministic=True,
+    random_state=10,
+
+)
+good_param = dict(
+    device_type='cpu',
+    boosting_type='gbdt',
+    num_threads=8,
+    n_jobs=None,
+    objective='regression',
+    metric='rmse',
+    lambda_l1=1000,
+    lambda_l2=1,
+    reg_alpha=None,
+    reg_lambda=None,
+    learning_rate=.1,
+    tree_learner='serial',
+    max_bin=128,
+    num_leaves=5,
+    max_depth=-1,
+
+    feature_fraction=1,  # .8
+
+    bagging_freq=1,
+    bagging_fraction=.8,
+    subsample=None,
+    subsample_freq=None,
+
+    min_child_weight=0.2,
+    min_data_in_leaf=2,
+    min_child_samples=None,
+    min_gain_to_split=None,
+    colsample_bytree=None,
+    min_split_gain=None,
+    n_estimators=10000,
+    verbose=-1,
+    deterministic=True,
+    random_state=10,
+
+)
 
 
-def run_gbt(x_train, y_train, x_test, y_test, feature_names, binarize=False):
-    param = dict(
-        device_type='cpu',
-        boosting='gbdt',
-        nthread=8,
-        objective='regression',
-        metric='rmse',
-        lambda_l1=1,
-        lambda_l2=1,
-        learning_rate=.01,
-        tree_learner='serial',
-        max_bin=63,
-        num_leaves=10,
-        max_depth=10,
-        feature_fraction=.5,
-        min_data_in_leaf=1,
-        min_gain_to_split=1,
-        verbose=-1
-
-    )
+def run_gbt(x_train, y_train, x_test, y_test, feature_names):
+    eval_metric = 'rmse'
     model_name = 'gbt'
 
-    if binarize:
-        param['objective'] = 'binary'
-        param['metric'] = 'auc'
-        y_train, y_test = convert_to_binary(y_train, y_test)
-        model_name = 'gbt_binary'
-
-    train_data = lgb.Dataset(x_train, label=y_train, feature_name=feature_names)
-    validation_data = lgb.Dataset(x_test, label=y_test, feature_name=feature_names)
-    num_round = 1000
-    bst = lgb.train(
-        param,
-        train_data,
-        num_round,
-        valid_sets=validation_data,
-        callbacks=[lgb.early_stopping(stopping_rounds=100, verbose=0)]
+    lgb_model = lgb.LGBMRegressor(**good_param)
+    lgb_model.fit(
+        x_train, y_train,
+        callbacks=[lgb.early_stopping(
+            500, first_metric_only=True, verbose=False)
+        ],
+        eval_metric=eval_metric,
+        eval_set=[(x_train, y_train), (x_test, y_test)],
     )
-    table = bst.feature_importance()
-    feats = pd.Series(table, index=feature_names)
+
+    feats = pd.Series(lgb_model.feature_importances_, index=feature_names)
     selected_feat = feats[feats > 0].index.values
 
-    preds = bst.predict(x_test, num_iteration=bst.best_iteration)
-    error, r2, pearson = score_all(y_test, preds)
-    auc = np.nan
-    if binarize:
-        auc = metrics.average_precision_score(y_test, preds)
+    preds = lgb_model.predict(x_test, num_iteration=lgb_model.best_iteration_)
+    error, r2, pearson, spearman, pr, sr = score_all(y_test, preds)
+
     return {
-        'mse': error,
-        'r2': r2,
-        'pearsonr': pearsonr(y_test, preds)[0],
         'model': model_name,
-        'feature_names': selected_feat,
-        'auc': auc
+        'feature_names': sorted(selected_feat),
+        'n_feat': len(selected_feat),
+        'rmse': error,
+        'r2': r2,
+        'pearson': pearson,
+        'spearman': spearman,
+        'pr': pr,
+        'sr': sr,
     }
 
 
 def score_all(y_test, preds):
     error = np.sqrt(metrics.mean_squared_error(y_test, preds))
     r2 = metrics.r2_score(y_test, preds)
-    pearson = pearsonr(y_test, preds)[0]
-    #     print(f"RMSE: {error:0.3f} | R^2 {r2:0.3f} | R {pearson:0.3f}")
-    return error, r2, pearson
+    pearson, pr = pearsonr(y_test, preds)
+    spearman, sr = spearmanr(y_test, preds)
+    return error, r2, pearson, spearman, pr, sr
 
 
 def run_model(d_sets, drug_name):
-    df_subset = data.get_trainable_data(d_sets, drug_name)
-    cols = list(set(df_subset.columns.values))
-    cols.remove(drug_name)
-
-    features = df_subset[cols].copy()
-    target = df_subset[drug_name].values
-
-    n_features_before = features.shape[1]
-    # features = features.loc[:, features.mean() > 0]
-    # features = features.loc[:, features.std() > 0]
-    n_features = features.shape[1]
-    print(f"Using {n_features} out of {n_features_before}"
-          f" ({n_features_before - n_features} removed)")
-    if features.shape[0] < 100:
-        return pd.DataFrame()
-    #     features = pd.DataFrame(features, columns=feature_names)
+    df_subset = data.get_trainable_data(d_sets, drug_name, new_format=True)
+    features = df_subset.features
+    target = df_subset.target
     feature_names = list(set(features.columns.values))
 
     all_results = []
-    kf = KFold(n_splits=5, shuffle=True, random_state=101)
+    # kf = KFold(n_splits=10, shuffle=True, random_state=101)
+    kf = RepeatedKFold(n_splits=5, n_repeats=5, random_state=101)
+
     for n, (train_index, test_index) in enumerate(kf.split(features)):
         x_train, x_test = features.iloc[train_index], features.iloc[test_index]
         y_train, y_test = target[train_index], target[test_index]
@@ -164,68 +178,93 @@ def run_model(d_sets, drug_name):
             x_test=x_test,
             y_test=y_test
         )
+
         gbt_results = run_gbt(feature_names=feature_names, **args)
-        gbt_binary_results = run_gbt(
-            feature_names=feature_names, binarize=True, **args
-        )
 
         enet_results = run_sklearn(
             model=en_model, model_name='EN', **args
         )
 
-        lasso_results = run_sklearn(
-            model=lasso_model, model_name='LASSO', **args
-        )
-        svm_results = run_sklearn(
-            model=svm_model, model_name='SVM', **args
-        )
-        svc_results = run_sklearn(
-            model=svm_model, model_name='SVC', binarize=True, **args
-        )
+        # svm_results = run_sklearn(
+        #     model=svm_model, model_name='SVM', **args
+        # )
 
         results = pd.DataFrame(
-            [gbt_results, gbt_binary_results, enet_results, lasso_results, svm_results, svc_results]
+            [enet_results, gbt_results]
         )
+
+        # results = pd.DataFrame([gbt_results])
         results['k'] = n
         all_results.append(results)
-        print(results[['model', 'mse', 'r2', 'pearsonr', 'auc']])
+        # print(results[['model', 'rmse', 'r2', 'pearson', 'auc']])
 
     if not isinstance(d_sets, str):
         out_name = '_'.join(sorted(d_sets))
     else:
         out_name = d_sets
     all_results = pd.concat(all_results)
-    all_results['drug'] = drug_name
+    all_results['drug_name'] = drug_name
     all_results['data_type'] = out_name
+    cols = ['model', 'n_feat', 'rmse', 'r2',
+            'pearson', 'spearman', ]
+
+    print('\t', d_sets, drug_name)
+    with pd.option_context("display.precision", 2):
+        print(all_results[cols])
     all_results.feature_names = all_results.feature_names.str.join('|')
-    return all_results[['model', 'k', 'mse', 'r2', 'pearsonr', 'auc', 'drug', 'data_type', 'feature_names']]
+    return all_results
 
 
 if __name__ == '__main__':
+    table = data.auc_table[data.drug_names].copy()
 
-    counts = data.auc_table[data.auc_table < 100].count().sort_values()
-    drugs = counts[counts > 20].index.values
+    # at least 100 samples
+    drug_counts = table.describe().T['count']
+    high_occ_drugs = drug_counts[drug_counts > 100].index.values
+
+    counts = table[table < 100].count()
+    responsive_drugs = counts[counts > 10].index.values
     # only run single drugs for now
-    drug_solo = [i for i in drugs if ' - ' not in i]
+    drug_solo = [i for i in responsive_drugs if ' - ' not in i]
+    drugs_to_focus = [
+        'Gilteritinib',
+        'Quizartinib (AC220)',
+        'Trametinib (GSK1120212)',
+        'Sorafenib',
+        'Panobinostat',
+        'Venetoclax',
+    ]
+    good_drugs = set(drug_solo).intersection(high_occ_drugs)
+    print(len(good_drugs))
+
+    # adding FLT3 inhibitors back in.
+    for i in drugs_to_focus:
+        if i not in good_drugs:
+            good_drugs.add(i)
+
     data_sources = [
-        'rna_seq',
         'proteomics',
+        'rna_seq',
         'phospho',
         'wes',
         ['proteomics', 'phospho'],
-        ['proteomics', 'rna_seq', ],
+        ['proteomics', 'rna_seq'],
         ['proteomics', 'wes'],
-        ['phospho', 'rna_seq', ],
+
+        ['phospho', 'rna_seq'],
         ['phospho', 'wes'],
         ['rna_seq', 'wes'],
-        ['phospho', 'rna_seq', 'wes'],
-        ['proteomics', 'rna_seq', 'wes'],
+
         ['proteomics', 'phospho', 'wes'],
+        ['rna_seq', 'phospho', 'wes'],
+        ['proteomics', 'phospho', 'rna_seq'],
+        ['proteomics', 'wes', 'rna_seq'],
+
         ['proteomics', 'phospho', 'rna_seq', 'wes'],
     ]
 
     models = []
-    for i in drug_solo:
+    for i in reversed(sorted(good_drugs)):
         print(f"Working on {i}")
         for j in data_sources:
             models.append(run_model(j, i))
@@ -234,4 +273,4 @@ if __name__ == '__main__':
         models,
     )
 
-    df.to_csv("ultimate_output_all_data.csv")
+    df.to_csv("regression_all_models_all_data_combos_cv_5v5.csv")
